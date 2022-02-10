a50ef3b5e1204ac94a19bba860ca5a813c02e168
Ariel <arielox.ag@gmail.com>
2022-02-09 23:35:52 -0300

    inicial

---
+++ client/src/components/Logo.js
@@ -0,0 +1,11 @@
+import logo from '../assets/images/logo.svg';
+
+const Logo = () => {
+   return (
+      <nav>
+         <img src={logo} alt="logo" className="logo" />
+      </nav>
+   );
+};
+
+export default Logo;
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,33 @@
+import { Logo } from '../components';
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+
+const Landing = () => {
+   return (
+      <Wrapper>
+         <nav>
+            <Logo />
+         </nav>
+
+         <div className="container page">
+            <div className="info">
+               <h1>
+                  job <span>tracking</span> app
+               </h1>
+               <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Voluptas dolor fugit earum, laudantium numquam quisquam
+                  molestias, quidem natus quibusdam dolores molestiae maiores
+                  vitae amet veritatis magnam assumenda sit, eaque fugiat?
+               </p>
+
+               <button className="btn btn-hero">Login/Register</button>
+            </div>
+
+            <img src={main} alt="job hunting" className="img main-img" />
+         </div>
+      </Wrapper>
+   );
+};
+
+export default Landing;
