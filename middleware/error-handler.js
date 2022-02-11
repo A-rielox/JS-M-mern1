29f1866c1b88d57151bed030628551bfa0a20940
Ariel <arielox.ag@gmail.com>
2022-02-11 00:58:52 -0300

    error handling listoco

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,21 +1,30 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-   console.log(err);
+   console.log(err.message);
 
    const defaultError = {
-      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-      msg: 'Something went wrong, try again later',
+      statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+      msg: err.message || 'Something went wrong, try again later',
    };
 
-   return res.status(defaultError.statusCode).json({ msg: err });
+   if (err.name === 'ValidationError') {
+      defaultError.msg = Object.values(err.errors)
+         .map(item => item.message)
+         .join(',');
+
+      defaultError.statusCode = StatusCodes.BAD_REQUEST;
+   }
+
+   if (err.code && err.code === 11000) {
+      defaultError.msg = `Duplicate value entered for ${Object.keys(
+         err.keyValue
+      )} field, please choose another value`;
+      defaultError.statusCode = StatusCodes.BAD_REQUEST;
+   }
+
+   return res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+   // return res.status(defaultError.statusCode).json({ msg: err });
 };
 
 export default errorHandlerMiddleware;
-
-// if (err.name === 'ValidationError') {
-//    customError.msg = Object.values(err.errors)
-//       .map(item => item.message)
-//       .join(',');
-//    customError.statusCode = 400;
-// }
