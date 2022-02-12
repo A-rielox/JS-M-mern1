d282fef97fe8d8df73efa9fc935f0126221b8c52
Ariel <arielox.ag@gmail.com>
2022-02-12 18:37:36 -0300

    listo logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,44 @@
+import { useState } from 'react';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import Logo from './Logo';
 import styled from 'styled-components';
 
 const Navbar = () => {
+   const { toggleSidebar, logoutUser, user } = useAppContext();
+   const [showLogout, setShowLogout] = useState(false);
+
    return (
       <Wrapper>
-         <h4>Navbar</h4>
+         <div className="nav-center">
+            <button className="toggle-btn" onClick={toggleSidebar}>
+               <FaAlignLeft />
+            </button>
+
+            <div>
+               <Logo />
+               <h3 className="logo-text">dashboard</h3>
+            </div>
+
+            <div className="btn-container">
+               <button
+                  className="btn"
+                  onClick={() => setShowLogout(!showLogout)}
+               >
+                  <FaUserCircle />
+                  {user?.name}
+                  <FaCaretDown />
+               </button>
+
+               <div
+                  className={`dropdown ${showLogout ? 'show-dropdown' : null} `}
+               >
+                  <button onClick={logoutUser} className="dropdown-btn">
+                     logout
+                  </button>
+               </div>
+            </div>
+         </div>
       </Wrapper>
    );
 };
