6fd0111f53a44a19f9318314269f7d0795c25c05
Ariel <arielox.ag@gmail.com>
2022-02-14 13:02:23 -0300

    listo create job en back

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,21 @@
+import Job from '../models/Job.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
+
 const createJob = async (req, res) => {
-   res.send('create jobbbbb');
+   const { position, company } = req.body;
+
+   if (!position || !company) {
+      throw new BadRequestError('Please provide all values');
+   }
+
+   req.body.createdBy = req.user.userId;
+
+   const job = await Job.create(req.body);
+
+   res.status(StatusCodes.CREATED).json({ job });
+
+   // el req.user se crea en authenticateUser from './middleware/auth.js
 };
 
 const deleteJob = async (req, res) => {
