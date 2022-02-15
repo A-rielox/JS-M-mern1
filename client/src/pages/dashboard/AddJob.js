679463dac5306af0dbca656d8d54b452aa240394
Ariel <arielox.ag@gmail.com>
2022-02-15 15:05:07 -0300

    listo updateJob back

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -43,7 +43,7 @@ const AddJob = () => {
          editJob();
          return;
       }
-
+         
       createJob();
    };
 
--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
+import checkPermissions from '../utils/checkPermissions.js';
 
 const createJob = async (req, res) => {
    const { position, company } = req.body;
@@ -33,7 +34,34 @@ const getAllJobs = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-   res.send('update jobbbbb');
+   const { id: jobId } = req.params;
+
+   const { company, position } = req.body; // ya lo reviso en front pero xsi a caso
+   if (!company || !position) {
+      throw new BadRequestError('Please Provide All Values');
+   }
+
+   const job = await Job.findOne({ _id: jobId });
+   if (!job) {
+      throw new NotFoundError(`No job with id ${jobId}`);
+   }
+
+   //check permissions
+   checkPermissions(req.user, job.createdBy);
+
+   const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+      new: true,
+      runValidators: true,
+   });
+
+   // 🔰
+   // job.position = position;
+   // job.company = company;
+   // await job.save();
+   // res.status(StatusCodes.OK).json({ job });
+
+   // no necesito el job-updated en el front pero pa verlo en postman
+   res.status(StatusCodes.OK).json({ updatedJob });
 };
 
 const showStats = async (req, res) => {
@@ -41,3 +69,7 @@ const showStats = async (req, res) => {
 };
 
 export { createJob, deleteJob, getAllJobs, updateJob, showStats };
+
+//
+// 🔰 la otra forma; con findOneAndUpdate NO tengo acceso a los hooks, como los "pre('save')", con esta forma SI
+// lo malo es q tengo q ponerlas todas aca abajo para q se actualicen, si se manda una y no la pongo aca abajo entonces obviamente no se va a actualizar, y tengo q pasar algun tipo de valor para los q sean required
